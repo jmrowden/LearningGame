b37413254276b053eeb0074999c42705fc46c212
JacquelineRowden <jmrowden@oakland.edu>
2017-09-20 16:20:50 -0400

    Update server.php

--- server.php
+++ server.php
@@ -13,7 +13,7 @@ $db = mysqli_connect('localhost', 'root', '', 'learninggame');
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
 	// receive all input values from the form
-	$userfullname = mysqli_real_escape_string($db, $_POST['username']);
+	$username = mysqli_real_escape_string($db, $_POST['username']);
 	$email = mysqli_real_escape_string($db, $_POST['email']);
 	$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 	$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
